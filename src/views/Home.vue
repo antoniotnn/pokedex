86aba7a300dd44474bd5c62b9830a990c0cdd19d
Antonio Pires <tonan@outlook.com.br>
2022-04-07 18:26:59 -0300

    More Animations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
 
           <div class="card-body bg-pokebola bg-normal">
             <div class="pokemon">
-              <transition name="flip">
+              <transition name="animacao1">
                 <img src="@/assets/imgs/pokemons/001.png" v-if="exibir">
               </transition>
 
